efe264b1f3ac7c25c25978f0b594c46aa564f7fe
Artem Malko <artem.malko@gmail.com>
2016-05-15 23:14:58 +0600

    TARS-CLI-52: add new option to add component.
    Now you cann add component, which is based on scheme file.
    Small refactoring.

--- bin/tars.js
+++ bin/tars.js
@@ -107,7 +107,10 @@ program
     .option('-i, --ie', 'Add component with general files + folder for IE')
     .option('-f, --full', 'Add component with all files and folders (assets folder, folder for IE and so on)')
     .option('-t, --template', 'Add component as a copy of _template component')
-    .option('-s, --scheme  [schemeFile]', 'Add component, which structure is based on scheme file')
+    .option(
+        '-s, --scheme [schemeFile]',
+        'Add component, which structure is based on scheme file'
+    )
     .option('-e, --empty', 'Add component without files')
     .option('--customPath <customPath>', 'Add component into custom folder')
     .option('--silent', 'Add component in silent mode, without promt')
